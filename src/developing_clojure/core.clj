1b54bd652b9d07bcfd21c7474685fd649edcb636
Ivan Ramos Biagioni <140509979+ivan-maze@users.noreply.github.com>
2025-01-25 14:44:23 -0300

    predicate functions

--- src/developing_clojure/core.clj
+++ src/developing_clojure/core.clj
@@ -1,6 +1,23 @@
 (ns developing-clojure.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn discounted-value
+  "Returns the value discounted 10% if the absolute value is > 100 "
+  [absolute-value]
+  (if (> absolute-value 100)
+    (let [discount-tax (/ 10 100)
+          discount         (* absolute-value discount-tax)]
+      (- absolute-value discount))
+    absolute-value))
+
+(println (discounted-value 1000))
+(println (discounted-value 100))
+
+; PREDICATE
+(defn apply-discount?
+  [absolute-value]
+  (if (> absolute-value 100)
+    true
+    false))
+
+(println (apply-discount? 100))
+(println (apply-discount? 1000))
--- src/developing_clojure/functions.clj
+++ src/developing_clojure/functions.clj
@@ -1,23 +1,24 @@
 (ns developing-clojure.functions)
+
 (defn apply-discount [absolute-value] ; defining the function name and its [parameter]
     (* absolute-value 0.9)) ; multiplying parameter for a decimal 
 (apply-discount 100) ; return the function result for a defined parameter 
 
 (defn apply-discount [absolute-value] 
-    (* absolute-value (- 1 0.1))) ; multiplaying parameter for the result of a subtratciton 
+    (* absolute-value (- 1 0.1))) ; multiplying parameter for the result of a subtraction
 (apply-discount 100)
 
-(defn apply-discount ; **** ATENTITION: NOT RECOMMENDED - BAD PRACTICE **** 
+(defn apply-discount ; **** ATTENTION: NOT RECOMMENDED - BAD PRACTICE ****
     "Apply a defined discount in an inserted parameter" ; Function description
     [absolute-value] ; Function parameter
-    (def discount 0.10) ; Deifines a Global Symbol **** ATENTITION: NOT RECOMMENDED - BAD PRACTICE - generates memory trash **** 
+    (def discount 0.10) ; Defines a Global Symbol **** ATTENTION: NOT RECOMMENDED - BAD PRACTICE - generates memory trash ****
     (* absolute-value (- 1 discount))) 
 (apply-discount 100)
 
-(defn apply-discount ; **** GOOD PRACTICE - Lolcal Symbol during the scpote () ****
+(defn apply-discount ; **** GOOD PRACTICE - Local Symbol during the scope () ****
     "Apply a defined discount in an inserted parameter" 
     [absolute-value] 
-    (let [discount 0.10] ; Creates a Local Symbol that recives a vector and existes during the scope ()
+    (let [discount 0.10] ; Creates a Local Symbol that receives a vector and exists during the scope ()
       (println "Calculating discount of" discount)
       (* absolute-value (- 1 discount)))) 
 (apply-discount 100) ; should return a double
@@ -25,13 +26,13 @@
 (defn apply-discount ; Trying different approach 
     "Apply a defined discount in an inserted parameter" 
     [absolute-value] 
-    (let [discount (/ 10 100)] ; Creates a Local Symbol that recives a vector, which is a result of a division, and existes during the scope ()
+    (let [discount (/ 10 100)] ; Creates a Local Symbol that receives a vector, which is a result of a division, and exists during the scope ()
       (println "Calculating discount of" discount)
       (* absolute-value (- 1 discount)))) 
 (apply-discount 100) ; should return 90N what is a BigInt (N = BigInt ; M = BigDecimal)
 
 (defn apply-discount ; Introducing conditional (if <condition> <return if true> <return if false>
-    "Apply a defined discount in an inserted parameter if absolute value is greatter than 100" 
+    "Apply a defined discount in an inserted parameter if absolute value is greater than 100"
     [absolute-value] 
     (if (> absolute-value 100) ; condition
       (let [discount 0.10] 
@@ -40,7 +41,7 @@
 (apply-discount 100) ; Return nill in case of false, once false is not defined
 
 (defn apply-discount ; Introducing conditional if (if <condition> <return if true> <return if false>
-    "Apply a defined discount in an inserted parameter if absolute value is greatter than 100" 
+    "Apply a defined discount in an inserted parameter if absolute value is greater than 100"
     [absolute-value] 
     (if (> absolute-value 100)
       (let [discount 0.10] 
